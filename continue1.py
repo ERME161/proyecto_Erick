be73db619b13e50e4e8729cd192f1e59efdaa32a
ERME161 <erick2001mendoza@gmail.com>
2022-03-02 09:35:15 -0500

    continue

---
+++ continue1.py
@@ -0,0 +1,6 @@
+
+cadena = 'Python'
+for letra in cadena:
+    if letra == 'P':
+        continue
+    print(letra)
---
+++ continue3.py
@@ -0,0 +1,7 @@
+
+x = 5
+while x > 0:
+    x -= 1
+    if x == 3:
+        continue
+    print(x)
