30ab9b54865d3a0e049a68c3da3d504af3f33a92
ERME161 <erick2001mendoza@gmail.com>
2022-03-02 09:25:45 -0500

    break

---
+++ break1.py
@@ -0,0 +1,7 @@
+
+cadena = 'Python'
+for letra in cadena:
+    if letra == 'h':
+        print("Se encontró la h")
+        break
+    print(letra)
---
+++ break2.py
@@ -0,0 +1,8 @@
+
+x = 5
+while True:
+    x -= 1
+    print(x)
+    if x == 0:
+        break
+    print("Fin del bucle")
