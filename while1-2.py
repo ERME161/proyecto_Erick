7e58c8d95626f3f9a911125bf0f089d138831127
ERME161 <erick2001mendoza@gmail.com>
2022-03-02 09:55:18 -0500

    while

---
+++ while1-2.py
@@ -0,0 +1,11 @@
+
+
+num = 11
+
+while num < 10 or num > 20 or num%2 !=0:
+    num = int(input('ingresar numero :'))
+
+print('.....')
+
+while not(num >= 10 or num <= 20 or num%2 ==0):
+    num = int(input('ingresar numero :'))
---
+++ while2.py
@@ -0,0 +1,5 @@
+numero = str(input('ingrese numero:'))
+while numero <= 10:
+    suma = numero + suma
+    numero = numero + 1
+print ("La suma es " + str(suma))
