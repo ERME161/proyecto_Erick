0d939493d326c977cb17c44762c37d61b570e946
ERME161 <erick2001mendoza@gmail.com>
2022-03-02 09:36:14 -0500

    diccionario

---
+++ dicc1.py
@@ -0,0 +1,5 @@
+
+diccionario = {'nombre' : 'Carlos', 'edad' : 22, 'cursos': ['Python','Django','JavaScript'] }
+print(diccionario['nombre']) 
+print(diccionario['cursos'])
+print(diccionario['cursos'][0])
